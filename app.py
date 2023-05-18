b3ce11f489ae5db74f414de80720c61ee1a3ebba
Prateek K. Keshari <prateekkeshari7@gmail.com>
2023-05-18 18:54:55 +0200

    updated app.py for q&a and faster captioning

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-from langchain.document_loaders import ImageCaptionLoader
-from langchain.indexes import VectorstoreIndexCreator
 from flask import Flask, jsonify, request, render_template
 import os
 import validators
 import requests
+import replicate
+from io import BytesIO
 
 app = Flask(__name__)
-
-# Set the static folder for Flask to serve static files from
 app.static_folder = 'templates/static'
-
-list_image_urls = []
-index = None
-
-os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/generate', methods=['POST'])
 def generate():
-    global index
-    if not index:
-        return jsonify({'error': 'Please upload an image before generating a caption.'}), 400
-
     image_url = request.form.get('image_url')
+    question = request.form.get('question')
 
     if not validate_image_url(image_url):
         return jsonify({'error': 'Invalid image URL.'}), 400
@@ -34,22 +25,31 @@ def generate():
         return jsonify({'error': 'The URL does not point to a valid image.'}), 400
 
     try:
-        loader = ImageCaptionLoader(path_images=[image_url])
-        list_docs = loader.load()
-        index = VectorstoreIndexCreator().from_loaders([loader])
-        result = index.query('Generate a long and descriptive with at least 3 sentences Instagram caption describing this image in first person. If a place is recognized, mention it. Include one emoji.')
-        return jsonify({'caption': result})
+        response = requests.get(image_url, stream=True)
+        response.raise_for_status()  # Check for any request errors
+
+        image_bytes = BytesIO(response.content)  # Read the response content into BytesIO object
+
+        input_data = {"image": image_bytes, "question": question} if question else {"image": image_bytes}
+
+        output = replicate.run(
+            "andreasjansson/blip-2:4b32258c42e9efd4288bb9910bc532a69727f9acd26aa08e175713a0a857a608",
+            input=input_data
+        )
+
+        return jsonify({'caption': output})
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        print(f"Error: {str(e)}")
+        return jsonify({'error': 'An internal server error occurred.'}), 500
+
+
 
 def validate_image_url(image_url):
-    # Validate image URL format
     if not validators.url(image_url):
         return False
     return True
 
 def is_valid_image(image_url):
-    # Check if the URL points to a valid image
     response = requests.head(image_url)
     content_type = response.headers.get('content-type')
     if content_type and 'image' in content_type:
