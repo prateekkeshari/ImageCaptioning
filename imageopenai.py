ab4fb238bd4585c4d43ae4375e159e0e6765e3fb
Prateek K. Keshari <prateekkeshari7@gmail.com>
2023-05-31 15:45:37 +0200

    added comments

--- imageopenai.py
+++ imageopenai.py
@@ -17,11 +17,18 @@ os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 @app.route('/')
 def index():
+    """
+    Render the index.html template for the homepage.
+    """
     return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
+    """
+    Handle the image caption generation request.
+    """
     global index
+
     if not index:
         return jsonify({'error': 'Please upload an image before generating a caption.'}), 400
 
@@ -34,22 +41,27 @@ def generate():
         return jsonify({'error': 'The URL does not point to a valid image.'}), 400
 
     try:
+        # Load the image and generate captions
         loader = ImageCaptionLoader(path_images=[image_url])
         list_docs = loader.load()
         index = VectorstoreIndexCreator().from_loaders([loader])
-        result = index.query('Describe this image in 2 sentences.')
+        result = index.query('Describe this image in 2 sentences.') #this is the instruction you are sending to openai
         return jsonify({'caption': result})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 def validate_image_url(image_url):
-    # Validate image URL format
+    """
+    Validate the format of the image URL.
+    """
     if not validators.url(image_url):
         return False
     return True
 
 def is_valid_image(image_url):
-    # Check if the URL points to a valid image
+    """
+    Check if the URL points to a valid image.
+    """
     response = requests.head(image_url)
     content_type = response.headers.get('content-type')
     if content_type and 'image' in content_type:
